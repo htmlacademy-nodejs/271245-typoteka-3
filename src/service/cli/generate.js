7bf4c70a335093789f0e729c385401c0104e6147
Polkovn1k <wa0s@narod.ru>
2022-05-15 20:57:10 +0300

    module1-task3

---
+++ src/service/cli/generate.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const fs = require(`fs`);
+const {getRandomInt, shuffle, createDate} = require(`../../utils.js`);
+const {TITLES, SENTENCES, CATEGORY} = require(`../../mock_data/mock_data.js`);
+const {ExitCode} = require(`../../constans.js`);
+
+const DEFAULT_COUNT = 1;
+const MAX_COUNT = 1000;
+const ANNOUNCE_MAX_COUNT = 5;
+const FILE_NAME = `mock.json`;
+const THREE_MONTH_TIMESTAMP = 86400000 * 7 * 4 * 3;
+
+const generatePublication = (count) => {
+  return Array(count).fill({}).map(() => {
+    return {
+      title: TITLES[getRandomInt(0, TITLES.length - 1)],
+      announce: shuffle(SENTENCES).slice(0, getRandomInt(0, ANNOUNCE_MAX_COUNT)).join(` `),
+      fullText: shuffle(SENTENCES).slice(0, getRandomInt(0, SENTENCES.length - 1)).join(` `),
+      createdDate: createDate(Date.now() - THREE_MONTH_TIMESTAMP, Date.now()),
+      сategory: shuffle(CATEGORY).slice(0, getRandomInt(0, CATEGORY.length - 1)),
+    };
+  });
+};
+
+module.exports = {
+  name: `--generate`,
+  run(args) {
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    if (countOffer >= MAX_COUNT) {
+      console.log(`Не больше 1000 публикаций`);
+      process.exit(ExitCode.error);
+    }
+    const content = JSON.stringify(generatePublication(countOffer));
+
+    fs.writeFile(FILE_NAME, content, (err) => {
+      if (err) {
+        return console.error(`Can't write data to file...`);
+      }
+      return console.info(`Operation success. File created.`);
+    });
+  },
+};
