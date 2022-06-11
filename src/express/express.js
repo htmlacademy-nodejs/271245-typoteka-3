e2ccf458dcd988a454120347d4d5fe37385bc1e1
Polkovn1k <wa0s@narod.ru>
2022-06-11 12:31:05 +0300

    templates upd

--- src/express/express.js
+++ src/express/express.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const myRoutes = require(`./routes/my`);
 const articlesRoutes = require(`./routes/articles`);
 const rootRoutes = require(`./routes/root`);
 
-const PUBLIC_DIR = `src/express/public`;
+const PUBLIC_DIR = `public`;
+const TEMPLATES_DIR = `templates`;
 
 const port = 8080;
 
@@ -19,11 +21,11 @@ app.use(`/my`, myRoutes);
 
 app.use(`/articles`, articlesRoutes);
 
-app.use(express.static(PUBLIC_DIR));
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.listen(port, () => {
   console.log(green(`Сервер создан, порт: ${port}`));
 });
 
-app.set(`views`, `./src/express/templates`);
+app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
 app.set(`view engine`, `pug`);
